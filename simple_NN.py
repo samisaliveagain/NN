3ff1bb29ea3248674c2cdcd50c2c2f58d4a253c3
Samyak Jain <samyakjain999@gmail.com>
2025-11-04 15:32:38 +0100

    added commments

--- simple_NN.py
+++ simple_NN.py
@@ -6,11 +6,15 @@ import torch.nn.functional as F
 class NormalNN(nn.Module):
     def __init__(self):
         super().__init__()
+
         self.fc1 = nn.Linear(1, 50)
         self.fc2 = nn.Linear(50, 50)
         self.fc3 = nn.Linear(50, 1)
 
     def forward(self, x):
+        # Layer 1: Linear + Activation
         x = F.relu(self.fc1(x))
+        # Layer 2: Linear + Activation
         x = F.relu(self.fc2(x))
+        # Layer 3: Final Linear (Output)
         return self.fc3(x)
